b8edb5c9b2a38c55e5330e6cf92a35a204f94fed
Kshitiz Shrestha <kshitizshrestha12345@gmail.com>
2025-08-28 08:15:01 +0545

    Configure authOptions with google client

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
+import SessionProvider from "next-auth"
 import Sidebar from "@/components/Sidebar";
 import "./globals.css";
 
---
+++ lib/auth.ts
@@ -0,0 +1,25 @@
+import { NextAuthOptions } from "next-auth";
+import GoogleProvider from "next-auth/providers/google";
+
+export const authOptions: NextAuthOptions = {
+  providers: [
+    GoogleProvider({
+      clientId: process.env.AUTH_GOOGLE_CLIENT || "",
+      clientSecret: process.env.AUTH_GOOGLE_SECRET || "",
+    }),
+  ],
+  pages: {
+    signIn: "/auth/login",
+  },
+  callbacks: {
+    async session({ session, token }) {
+      return session;
+    },
+    async jwt({ token, user }) {
+      return token;
+    },
+  },
+  session: {
+    strategy: "jwt",
+  },
+};
